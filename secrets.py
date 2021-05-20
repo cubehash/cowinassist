6a24ec2b6eb4da057adde52c5349773e56394ab9
cubehash <53764767+cubehash@users.noreply.github.com>
2021-05-20 17:03:54 +0530

    token updates

---
+++ secrets.py
@@ -0,0 +1,6 @@
+# this is the bot token, gotten from BotFather
+TELEGRAM_BOT_TOKEN = "1883132425:AAECYt_yoXGMsemxbzlE2QVraVhz-S9GnN0"
+# this is your own telegram ID, which is used to send exception tracebacks
+DEVELOPER_CHAT_ID = "1670573144"
+# list of telegram IDs for them /stats command is enabled
+MAINTAINERS_CHAT_IDS = [1670573144, ]
